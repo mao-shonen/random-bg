e94059aaf24ce88e3a512df35d08f780b7984f4b
魔王少年 <q267009886.work@gmail.com>
2020-12-20 19:54:33 +0000

    初次提交

---
+++ README.md
@@ -0,0 +1,36 @@
+# random-bg
+
+~~又是一個沒找到合用的開源專案只好自己做系列~~
+一個簡單的隨機背景圖
+在我家的NAS上執行
+
+效果範例網址 `random-bg.maou.app/demo`
+
+> 圖片轉換使用 [cloudimage.io](https://cloudimage.io)
+> 使用需註冊一個cloudimage.io帳號
+
+
+## 安裝
+
+* python >= 3.7
+* [poetry](https://github.com/python-poetry/poetry)
+
+```bash
+poetry install
+```
+
+## 配置 dotenv
+
+`vim .env`
+
+```conf
+cloudimg_io_token={cloudimg.io拿到的token}
+image_dir={圖片資料夾}
+```
+
+## 執行
+
+[uvicorn](https://www.uvicorn.org)
+```bash
+uvicorn main:app --host 0.0.0.0 --port 8000
+```
---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+import random
+import hashlib
+from typing import Any, Dict, Optional
+from functools import lru_cache
+from urllib.parse import urlencode
+import httpx
+import aiofiles
+from pydantic import BaseSettings
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import RedirectResponse, FileResponse
+
+
+class Config(BaseSettings):
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+    cloudimg_io_token: str = None
+    image_dir: str = None
+    cache_dir: str = './cache'
+
+
+config = Config()
+if not all([config.cloudimg_io_token, config.image_dir]):
+    raise Exception('缺少config')
+
+app = FastAPI()
+
+
+def getImageUrl(domain: str, img: str) -> str:
+    return f'https://{config.cloudimg_io_token}.cloudimg.io/v7/{domain}/origin_img/{img}?' + urlencode(
+        dict(
+            func='crop',
+            width=1920,
+            height=1080,
+            sharp=1,
+            force_format='webp',
+            q=85,
+        ))
+
+
+@lru_cache()
+def md5(v: str) -> str:
+    return hashlib.md5(v.encode()).hexdigest()
+
+
+@lru_cache()
+def getImages(*, cache_ttl: Optional[Any] = None) -> Dict[str, str]:
+    # cloudimg.io 如果缺少副檔有機率出現錯誤
+    return {
+        md5(f) + os.path.splitext(f)[1]: f
+        for f in os.listdir(config.image_dir)
+        if f.endswith(('.jpg', '.jpeg', '.jfif', '.png', '.bmp', '.ico',
+                       '.gif', '.webp'))
+    }
+
+
+@app.get('/random')
+async def _random():
+    img_hash = random.choice(list(getImages().keys()))
+    url_path = f'/img/{img_hash}'
+    return RedirectResponse(url_path, status_code=302)
+
+
+@app.get('/imgs')
+async def _list():
+    return [dict(url=f'/img/{img_hash}') for img_hash in getImages().keys()]
+
+
+@app.get('/demo')
+async def _demo():
+    return FileResponse('demo.html')
+
+
+@app.get('/cache')
+async def _cache():
+    return FileResponse('cache.html')
+
+
+@app.get('/img/{img_hash}')
+async def _fetch(request: Request, img_hash: str):
+    img_path = getImages().get(img_hash)
+    if not img_path:
+        raise HTTPException(404)
+
+    cache_img_path = os.path.join(config.cache_dir, f'{img_hash}.webp')
+
+    if not os.path.exists(cache_img_path):
+        domain = request.headers.get('host', request.base_url.hostname)
+        async with httpx.AsyncClient() as client:
+            res = await client.get(getImageUrl(domain, img_hash), timeout=180)
+            res.raise_for_status()
+
+            tmp_file = cache_img_path + '.tmp'
+            async with aiofiles.open(tmp_file, 'wb') as f:
+                await f.write(res.read())
+
+            os.rename(tmp_file, cache_img_path)
+
+    return FileResponse(cache_img_path, media_type='image/webp')
+
+
+@app.get('/origin_img/{img_hash}')
+async def _origin_image(img_hash: str):
+    img_path = getImages().get(img_hash)
+    if not img_path:
+        raise HTTPException(404)
+
+    return FileResponse(os.path.join(config.image_dir, img_path))
